4651c7e948e827b736e4d05a506fb8d4e8c04ce7
matuMasaru <matu.esquivel94@gmail.com>
2020-07-21 17:55:52 -0300

    [matias] feat: Pregunta implementacion calculo puntaje

--- src/main/java/poo/coders/Main/Pregunta.java
+++ src/main/java/poo/coders/Main/Pregunta.java
@@ -3,24 +3,26 @@ package poo.coders.Main;
 public class Pregunta {
 	private Comportamiento tipoComportamiento;
 	private String pregunta;
-	private Puntos diferencialPuntos; //n
 	private Respuesta respuesta ;
 
 	public Pregunta(String pregunta, Respuesta respuesta, Comportamiento comportamiento){
 		this.tipoComportamiento = comportamiento;
 		this.pregunta = pregunta;
-		diferencialPuntos = new Puntos();
-		this.respuesta =  respuesta ;
+		this.respuesta = respuesta ;
 	}
 
-	private void evaluarRespuesta(Respuesta respuesta){
-		respuesta.aplicarComportamiento( tipoComportamiento, diferencialPuntos );
+
+	private Puntos evaluarRespuesta(Respuesta respuesta){
+		Puntos puntajeRespuesta = new Puntos();
+		respuesta.aplicarComportamiento( tipoComportamiento, puntajeRespuesta );
+		return puntajeRespuesta ;
 	}
 
-	public void evaluarRespuesta(Respuesta[] respuestas){
+	public Puntos calcularPuntaje(Respuesta[] respuestas){
+		Puntos puntajeTotalPregunta = new Puntos();
 		for (Respuesta unaRespuesta : respuestas){
-			this.evaluarRespuesta(unaRespuesta);
-
+			puntajeTotalPregunta.sumar(this.evaluarRespuesta(unaRespuesta));
 		}
+		return puntajeTotalPregunta;
 	}
 }
