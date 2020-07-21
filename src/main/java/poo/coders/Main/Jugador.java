c651ba01fd79826c48dd2aa9223eb33c7df50d3a
matuMasaru <matu.esquivel94@gmail.com>
2020-07-21 15:14:12 -0300

    feat: clase jugador

---
+++ src/main/java/poo/coders/Main/Jugador.java
@@ -0,0 +1,18 @@
+package poo.coders.Main;
+
+public class Jugador {
+
+	private String nombre;
+	private Puntos totalPuntos;
+
+	public Jugador(String nombre){
+		this.nombre = nombre;
+		totalPuntos = new Puntos();
+	}
+
+	public void modificarPuntos(Puntos diferencialPuntos){
+
+	}
+
+
+}
