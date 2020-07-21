9c711ea436215cf85796bb734491e4905b80d00e
matuMasaru <matu.esquivel94@gmail.com>
2020-07-21 15:16:36 -0300

    feat: clase Pregunta

---
+++ src/main/java/poo/coders/Main/Pregunta.java
@@ -0,0 +1,13 @@
+package poo.coders.Main;
+
+public class Pregunta {
+
+	private Puntos evaluarRespuesta(Respuesta respuesta){
+
+
+	}
+	public void evaluarRespuesta(Respuesta respuesta[]){
+
+
+	}
+}
