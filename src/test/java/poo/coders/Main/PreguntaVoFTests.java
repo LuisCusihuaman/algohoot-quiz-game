163de53dddd1cdc0d4205ad741164c402240224a
Ezequiel Schejtman <13641204+Ezequiel-Schejtman@users.noreply.github.com>
2020-07-21 23:52:31 -0300

    test: creo pregunta VoF indicando cual es la respuesta correcta

--- src/test/java/poo/coders/Main/PreguntaVoFTests.java
+++ src/test/java/poo/coders/Main/PreguntaVoFTests.java
@@ -1,4 +1,22 @@
 package poo.coders.Main;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class PreguntaVoFTests {
+
+	@Test
+	void test01CreoPreguntaVoFIndicandoCualEsLaRespuestaCorrecta(){
+		Jugador jugador = new Jugador("Pepito");
+		Puntos diferencialPuntos = new Puntos();
+
+		Pregunta pregunta = new Pregunta("Este es el mejor TP.", new ComportamientoVoF());
+		RespuestaCorrecta correcta = new RespuestaCorrecta();
+
+		correcta.aplicarComportamiento(new ComportamientoVoF(), diferencialPuntos);
+
+		Assertions.assertEquals(1, diferencialPuntos.getCantidadPuntos());
+
+	}
 }
