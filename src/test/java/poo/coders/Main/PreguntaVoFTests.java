0bf5b17ff4fb85ef2a90d4620091b3363b22bafe
Ezequiel Schejtman <13641204+Ezequiel-Schejtman@users.noreply.github.com>
2020-07-22 00:55:56 -0300

    test(PreguntaVoFTests): codigo de test02

--- src/test/java/poo/coders/Main/PreguntaVoFTests.java
+++ src/test/java/poo/coders/Main/PreguntaVoFTests.java
@@ -22,9 +22,39 @@ public class PreguntaVoFTests {
 
 	}
 
+	/*
+			P1 (Multiselect):
+				J1: [Respuesta1, Respuesta2]
+				J2: [Respuesta2, Respuesta4]
+
+			P2 (VoF):
+				J1: [Respuesta1]
+				J2: [Respuesta2]
+	*/
+
 	@Test
 	void test02PreguntaVoFRecibeListaDeRespuestasYAsignaPuntosCorrectamente(){
+		Jugador jugador1 = new Jugador("Tomas");
+		Jugador jugador2 = new Jugador("Kevin");
 
+		Pregunta pregunta = new Pregunta("Este enunciado es falso.",new ComportamientoVoF());
 
+		RespuestaCorrecta correcta = new RespuestaCorrecta();
+		RespuestaIncorrecta incorrecta = new RespuestaIncorrecta();
+
+		ArrayList<Respuesta> respuestaJugador1 = new ArrayList<>();
+		ArrayList<Respuesta> respuestaJugador2 = new ArrayList<>();
+
+		respuestaJugador1.add(correcta);
+		respuestaJugador2.add(incorrecta);
+
+		Puntos diferencialJugador1 = pregunta.obtenerPuntaje(respuestaJugador1);
+		jugador1.modificarPuntos(diferencialJugador1);
+
+		Puntos diferencialJugador2 = pregunta.obtenerPuntaje(respuestaJugador2);
+    	jugador2.modificarPuntos(diferencialJugador2);
+
+    	Assertions.assertEquals(1, jugador1.getPuntos());
+    	Assertions.assertEquals(0, jugador2.getPuntos());
 	}
 }
