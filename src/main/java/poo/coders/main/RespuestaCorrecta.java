7ec06a1812a9ac5c41779754d483493f9c79d36c
Ezequiel Schejtman <13641204+Ezequiel-Schejtman@users.noreply.github.com>
2020-07-22 22:13:07 -0300

    refactor(Respuesta): Respuesta pasa a ser abstracta

--- src/main/java/poo/coders/main/RespuestaCorrecta.java
+++ src/main/java/poo/coders/main/RespuestaCorrecta.java
@@ -1,8 +1,21 @@
 package poo.coders.main;
 
-public class RespuestaCorrecta implements Respuesta {
+public class RespuestaCorrecta extends Respuesta {
+
+	public RespuestaCorrecta() {
+		super();
+	}
+
+	public RespuestaCorrecta(String textoRespuesta) {
+		super(textoRespuesta);
+	}
+
 	@Override
 	public void aplicarComportamiento(Comportamiento comportamiento, Puntos puntos) {
 		comportamiento.correcto(puntos);
+	}
+
+	public String getTextoRespuesta(){
+		return this.textoRespuesta;
 	}
 }
--- src/main/java/poo/coders/main/RespuestaIncorrecta.java
+++ src/main/java/poo/coders/main/RespuestaIncorrecta.java
@@ -1,8 +1,21 @@
 package poo.coders.main;
 
-public class RespuestaIncorrecta implements Respuesta {
+public class RespuestaIncorrecta extends Respuesta {
+
+	public RespuestaIncorrecta() {
+		super();
+	}
+
+	public RespuestaIncorrecta(String textoRespuesta) {
+		super(textoRespuesta);
+	}
+
 	@Override
 	public void aplicarComportamiento(Comportamiento comportamiento, Puntos puntos) {
 		comportamiento.erroneo(puntos);
+	}
+
+	public String getTextoRespuesta(){
+		return this.textoRespuesta;
 	}
 }
