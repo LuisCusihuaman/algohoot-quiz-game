1ac495107d15cb82212e5fda658d5d3de6ace7ae
Kevin Vasquez <kvasquez@baufest.com>
2020-07-25 15:50:59 -0300

    chore(MultipleChoiceParcialTests): se crea clase para los tests de MultipleChoiceParcial

---
+++ src/test/java/poo/coders/main/MultipleChoiceParcialITests.java
@@ -0,0 +1,12 @@
+package poo.coders.main;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class MultipleChoiceParcialITests {
+	@Test
+	public void test01JugadorContestaCon3PreguntasCorrectasGana3Puntos(){
+	}
+}
