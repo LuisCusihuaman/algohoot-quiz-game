ef0c53d3f400dfe810dc92d6c464c694f2e35d66
Kevin Vasquez <kvasquez@baufest.com>
2020-07-25 15:55:33 -0300

    test: se añadio test test01JugadorContestaCon3PreguntasCorrectasGana3Puntos

--- src/test/java/poo/coders/main/MultipleChoiceParcialITests.java
+++ src/test/java/poo/coders/main/MultipleChoiceParcialITests.java
@@ -8,5 +8,20 @@ import java.util.ArrayList;
 public class MultipleChoiceParcialITests {
 	@Test
 	public void test01JugadorContestaCon3PreguntasCorrectasGana3Puntos(){
+		//	Arrange
+		Pregunta pregunta = new Pregunta("Pregunta", new MultipleChoiceParcial());
+		Jugador jugador = new Jugador("Jugador");
+
+		ArrayList<Respuesta> respuestas = new ArrayList<>();
+
+		respuestas.add(new RespuestaCorrecta());
+		respuestas.add(new RespuestaCorrecta());
+		respuestas.add(new RespuestaCorrecta());
+
+		//	Act
+		jugador.procesarPregunta(pregunta, respuestas);
+
+		//	Assert
+		Assertions.assertEquals(3, jugador.getPuntos());
 	}
 }
