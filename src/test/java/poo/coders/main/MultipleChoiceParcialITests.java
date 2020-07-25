94096b1c79453af68f29f4518b64013ac95ad0f5
Kevin Vasquez <kvasquez@baufest.com>
2020-07-25 16:02:24 -0300

    test: se añadio test test02JugadorContestaCon2PreguntasCorrectasYUnaIncorrectaAlFinalTienePuntos0

--- src/test/java/poo/coders/main/MultipleChoiceParcialITests.java
+++ src/test/java/poo/coders/main/MultipleChoiceParcialITests.java
@@ -3,6 +3,7 @@ package poo.coders.main;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MultipleChoiceParcialITests {
@@ -23,5 +24,17 @@ public class MultipleChoiceParcialITests {
 
 		//	Assert
 		Assertions.assertEquals(3, jugador.getPuntos());
+	}
+	@Test
+	public void test02JugadorContestaCon2PreguntasCorrectasYUnaIncorrectaAlFinalTienePuntos0(){
+		Pregunta pregunta = new Pregunta("Pregunta", new MultipleChoiceParcial());
+		Jugador jugador = new Jugador("Jugador");
+
+		ArrayList<Respuesta>respuestas = new ArrayList<>();
+		respuestas.add(new RespuestaCorrecta());
+		respuestas.add(new RespuestaCorrecta());
+		respuestas.add(new RespuestaIncorrecta());
+
+		Assertions.assertEquals(0, jugador.getPuntos());
 	}
 }
