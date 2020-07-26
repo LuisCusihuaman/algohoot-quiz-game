b4683a7c0cf5e10cb6f769ce1d887a1b2a2d42ff
matiasezequielcabral <cabralmatiasezequiel@gmail.com>
2020-07-26 03:03:13 -0300

    style: reformat code

--- src/main/java/poo/coders/main/ComportamientoMultipleChoiceClasico.java
+++ src/main/java/poo/coders/main/ComportamientoMultipleChoiceClasico.java
@@ -1,15 +1,15 @@
 package poo.coders.main;
 
-public class ComportamientoMultipleChoiceClasico implements Comportamiento{
+public class ComportamientoMultipleChoiceClasico implements Comportamiento {
 
 	@Override
-	public void correcto(Puntos puntos){
+	public void correcto(Puntos puntos) {
 		puntos.sumar();
 		puntos.setFactor(0);
 	}
 
 	@Override
-	public void incorrecto(Puntos puntos){
+	public void incorrecto(Puntos puntos) {
 		puntos.anular();
 	}
 }
