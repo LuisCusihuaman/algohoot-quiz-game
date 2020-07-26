055a6e88acfd27664299d62309d75f0e0219a949
matiasezequielcabral <cabralmatiasezequiel@gmail.com>
2020-07-26 02:32:38 -0300

    test: se creo la prueba test03JugadorContesta4CorrectamenteY1IncorrectamenteYRecibe0Punto

--- src/test/java/poo/coders/main/MultipleChoiceClasicoTest.java
+++ src/test/java/poo/coders/main/MultipleChoiceClasicoTest.java
@@ -5,19 +5,53 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-public class MultipleChoiceClasicoTest{
-
+public class MultipleChoiceClasicoTest {
 	@Test
-	public void test01JugadorResponde2RespuestasCorrectasYsuma2Puntos(){
+	public void test01JugadorContestaTodoCorrectamenteYRecibe1Punto(){
 		Pregunta pregunta = new Pregunta("Pregunta", new ComportamientoMultipleChoiceClasico());
 		Jugador jugador = new Jugador("Pepe");
 
 		ArrayList<Respuesta> respuestas = new ArrayList<>();
 		respuestas.add(new RespuestaCorrecta());
 		respuestas.add(new RespuestaCorrecta());
+		respuestas.add(new RespuestaCorrecta());
 
 		jugador.procesarPregunta(pregunta, respuestas);
 
-		Assertions.assertEquals(2, jugador.getPuntos());
+		Assertions.assertEquals(1, jugador.getPuntos());
 	}
+
+	@Test
+	public void test02JugadorContesta2CorrectamenteY1IncorrectamenteYRecibe0Punto(){
+		Pregunta pregunta = new Pregunta("Pregunta", new ComportamientoMultipleChoiceClasico());
+		Jugador jugador = new Jugador("Pepe");
+
+		ArrayList<Respuesta> respuestas = new ArrayList<>();
+		respuestas.add(new RespuestaCorrecta());
+		respuestas.add(new RespuestaIncorrecta());
+		respuestas.add(new RespuestaCorrecta());
+
+		jugador.procesarPregunta(pregunta, respuestas);
+
+		Assertions.assertEquals(0, jugador.getPuntos());
+	}
+
+	@Test
+	public void test03JugadorContesta4CorrectamenteY1IncorrectamenteYRecibe0Punto(){
+		Pregunta pregunta = new Pregunta("Pregunta", new ComportamientoMultipleChoiceClasico());
+		Jugador jugador = new Jugador("Pepe");
+
+		ArrayList<Respuesta> respuestas = new ArrayList<>();
+		respuestas.add(new RespuestaCorrecta());
+		respuestas.add(new RespuestaCorrecta());
+		respuestas.add(new RespuestaCorrecta());
+		respuestas.add(new RespuestaCorrecta());
+		respuestas.add(new RespuestaIncorrecta());
+
+		jugador.procesarPregunta(pregunta, respuestas);
+
+		Assertions.assertEquals(0, jugador.getPuntos());
+	}
+
+
 }
