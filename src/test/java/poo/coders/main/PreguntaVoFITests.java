8b360ad7dbf69c2d9a9f51c92a4ea3f82b0dd2ba
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-27 02:18:11 -0300

    refactor(VOFITests): separado test unitarios de respuesta y pregunta

--- src/test/java/poo/coders/main/PreguntaVoFITests.java
+++ src/test/java/poo/coders/main/PreguntaVoFITests.java
@@ -5,22 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-
 public class PreguntaVoFITests {
 
 	@Test
 	void test01CreoPreguntaVoFIndicandoCualEsLaRespuestaCorrecta() {
-		Pregunta pregunta = new Pregunta("Este es el mejor TP.", new ComportamientoVoF());
-		RespuestaCorrecta correcta = new RespuestaCorrecta();
-
+		Pregunta             pregunta        = new Pregunta("Este es el mejor TP.", new ComportamientoVoF());
+		RespuestaCorrecta    correcta        = new RespuestaCorrecta();
 		ArrayList<Respuesta> listaRespuestas = new ArrayList<>();
 		listaRespuestas.add(correcta);
 		Puntos diferencialPuntos = pregunta.obtenerPuntaje(listaRespuestas);
-
 		Assertions.assertEquals(1, diferencialPuntos.getCantidadPuntos());
-
 	}
-
 	/*
 			P1 (Multiselect):
 				J1: [Respuesta1, Respuesta2]
@@ -33,37 +28,20 @@ public class PreguntaVoFITests {
 
 	@Test
 	void test02PreguntaVoFRecibeListaDeRespuestasYAsignaPuntosCorrectamente() {
-		Jugador jugador1 = new Jugador("Tomas");
-		Jugador jugador2 = new Jugador("Kevin");
-
-		Pregunta pregunta = new Pregunta("Este enunciado es falso.", new ComportamientoVoF());
-
-		RespuestaCorrecta correcta = new RespuestaCorrecta();
-		RespuestaIncorrecta incorrecta = new RespuestaIncorrecta();
-
+		Jugador              jugador1          = new Jugador("Tomas");
+		Jugador              jugador2          = new Jugador("Kevin");
+		Pregunta             pregunta          = new Pregunta("Este enunciado es falso.", new ComportamientoVoF());
+		RespuestaCorrecta    correcta          = new RespuestaCorrecta();
+		RespuestaIncorrecta  incorrecta        = new RespuestaIncorrecta();
 		ArrayList<Respuesta> respuestaJugador1 = new ArrayList<>();
 		ArrayList<Respuesta> respuestaJugador2 = new ArrayList<>();
 
-
 		respuestaJugador1.add(correcta);
 		respuestaJugador2.add(incorrecta);
-
 		jugador1.procesarPregunta(pregunta, respuestaJugador1);
 		jugador2.procesarPregunta(pregunta, respuestaJugador2);
 
 		Assertions.assertEquals(1, jugador1.getPuntos());
 		Assertions.assertEquals(0, jugador2.getPuntos());
-	}
-
-	@Test
-	void test03DeberianAsignarseLosTextosAPreguntaYRespuestaCorrectamente() {
-		Pregunta pregunta = new Pregunta("Vamos a aprobar el TP.", new ComportamientoVoF());
-
-		RespuestaCorrecta correcta = new RespuestaCorrecta("Verdadero.");
-		RespuestaIncorrecta incorrecta = new RespuestaIncorrecta("Falso.");
-
-		Assertions.assertEquals("Vamos a aprobar el TP.", pregunta.getTextoPregunta());
-		Assertions.assertEquals("Verdadero.", correcta.getTextoRespuesta());
-		Assertions.assertEquals("Falso.", incorrecta.getTextoRespuesta());
 	}
 }
