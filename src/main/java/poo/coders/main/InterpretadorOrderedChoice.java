c275bc60e68423cdc81a555ada904cf70f88689d
Ezequiel Schejtman <13641204+Ezequiel-Schejtman@users.noreply.github.com>
2020-08-03 23:47:21 -0300

    feat(InterpretadorOrderedChoice): evaluarRespuestas

--- src/main/java/poo/coders/main/InterpretadorOrderedChoice.java
+++ src/main/java/poo/coders/main/InterpretadorOrderedChoice.java
@@ -1,4 +1,12 @@
 package poo.coders.main;
 
+import java.util.List;
+
 public class InterpretadorOrderedChoice {
+	List<String> respuestasOrdenadas;
+
+	Respuesta evaluarRespuestas(List<String> respuestasJugador){
+		if(respuestasOrdenadas.equals(respuestasJugador)) return new RespuestaCorrecta("");
+		return new RespuestaIncorrecta("");
+	}
 }
