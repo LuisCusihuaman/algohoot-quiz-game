50ae9c6720209ed340bc9ee0f1b95ca65bf8084a
Ezequiel Schejtman <13641204+Ezequiel-Schejtman@users.noreply.github.com>
2020-08-04 00:08:11 -0300

    test(InterpretadorOrderedChoiceTest): Una respuesta desordenada devuelve respuesta incorrecta

--- src/test/java/poo/coders/main/InterpretadorOrderedChoiceTest.java
+++ src/test/java/poo/coders/main/InterpretadorOrderedChoiceTest.java
@@ -7,12 +7,22 @@ import java.util.List;
 
 public class InterpretadorOrderedChoiceTest {
 	@Test
-	void test01UnaRespuestasOrdenadaDevuelveRespuestaCorrecta(){
+	void test01UnaRespuestaOrdenadaDevuelveRespuestaCorrecta(){
 		List<String> respuestasOrdenadas = List.of("Orden 1", "Orden 2", "Orden 3");
 		List<String> respuestasJugador = List.of("Orden 1", "Orden 2", "Orden 3");
 
 		InterpretadorOrderedChoice interpretador = new InterpretadorOrderedChoice(respuestasOrdenadas);
 
 		Assertions.assertEquals("Correcta", interpretador.evaluarRespuestas(respuestasJugador).getTextoRespuesta());
+	}
+
+	@Test
+	void test02UnaRespuestaDesordenadaDevuelveRespuestaIncorrecta(){
+		List<String> respuestasOrdenadas = List.of("Orden 1", "Orden 2", "Orden 3");
+		List<String> respuestasJugador = List.of("Orden 2", "Orden 1", "Orden 3");
+
+		InterpretadorOrderedChoice interpretador = new InterpretadorOrderedChoice(respuestasOrdenadas);
+
+		Assertions.assertEquals("Incorrecta", interpretador.evaluarRespuestas(respuestasJugador).getTextoRespuesta());
 	}
 }
