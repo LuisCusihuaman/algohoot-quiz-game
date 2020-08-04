ba4a6d366c155171790561ed81fab99d1626f193
matiasezequielcabral <cabralmatiasezequiel@gmail.com>
2020-08-04 12:46:13 -0300

    test: JugadorResponde3OpcionesCorrectasYRecibe3Puntos

---
+++ src/test/java/poo/coders/main/ComportamientoMultipleChoicePenalidadTest.java
@@ -0,0 +1,26 @@
+package poo.coders.main;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class ComportamientoMultipleChoicePenalidadTest{
+
+	@Test
+	public void test01JugadorResponde3OpcionesCorrectasYRecibe3Puntos(){
+
+		Pregunta pregunta = new Pregunta("Pregunta", new ComportamientoMultipleChoicePenalidad());
+		Jugador jugador = new Jugador("Pepe");
+		ArrayList<Respuesta> respuestas = new ArrayList<>();
+
+		respuestas.add(new RespuestaCorrecta(""));
+		respuestas.add(new RespuestaCorrecta(""));
+		respuestas.add(new RespuestaCorrecta(""));
+
+		jugador.procesarPregunta(pregunta, respuestas);
+
+		Assertions.assertEquals(3, jugador.getPuntos());
+
+	}
+}
