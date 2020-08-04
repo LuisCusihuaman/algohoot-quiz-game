c63b924218951035cbf47f9e81ea87a09929087f
Ezequiel Schejtman <13641204+Ezequiel-Schejtman@users.noreply.github.com>
2020-08-03 23:59:40 -0300

    feat(InterpretadorOrderedChoice): constructor con lista de respuestasOrdenadas

--- src/main/java/poo/coders/main/InterpretadorOrderedChoice.java
+++ src/main/java/poo/coders/main/InterpretadorOrderedChoice.java
@@ -5,8 +5,12 @@ import java.util.List;
 public class InterpretadorOrderedChoice {
 	List<String> respuestasOrdenadas;
 
+	InterpretadorOrderedChoice(List<String> respuestasOrdenadas){
+		this.respuestasOrdenadas = respuestasOrdenadas;
+	}
+
 	Respuesta evaluarRespuestas(List<String> respuestasJugador){
-		if(respuestasOrdenadas.equals(respuestasJugador)) return new RespuestaCorrecta("");
-		return new RespuestaIncorrecta("");
+		if(respuestasOrdenadas.equals(respuestasJugador)) return new RespuestaCorrecta("Correcta");
+		return new RespuestaIncorrecta("Incorrecta");
 	}
 }
