3a925e2a7e8ab64ab3293043194262696dee91c5
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-04 00:29:29 -0300

    test(ComportamientoOrderedChoiceTest): se añadio test01OrdenCorrectoEsABCYElJugadorRespondeConABCEntoncesTienePuntaje1

---
+++ src/test/java/poo/coders/main/ComportamientoOrderedChoiceITest.java
@@ -0,0 +1,21 @@
+package poo.coders.main;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class ComportamientoOrderedChoiceITest {
+	@Test
+	public void test01OrdenCorrectoEsABCYElJugadorRespondeConABCEntoncesTienePuntaje1() {
+		Pregunta pregunta = new Pregunta("", new ComportamientoOrderedChoice());
+		Jugador jugador = new Jugador("");
+		List<String> textoRespuestasOrdenadas = List.of("A", "B", "C");
+		List<String> textoRespuestasJugador = List.of("A", "B", "C");
+		InterpretadorOrderedChoice interpretador = new InterpretadorOrderedChoice(textoRespuestasOrdenadas);
+
+		jugador.procesarPregunta(pregunta, interpretador.evaluarRespuestas(textoRespuestasJugador));
+
+		Assertions.assertEquals(1, jugador.getPuntos());
+	}
+}
