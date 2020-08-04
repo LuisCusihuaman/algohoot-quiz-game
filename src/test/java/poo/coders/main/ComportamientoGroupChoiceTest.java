e9386b192f5fba91ce4e9e560c7637b68c583cd6
matiasezequielcabral <cabralmatiasezequiel@gmail.com>
2020-08-04 14:52:25 -0300

    test: JugadorColocaMitadDeLasRespuestasCorrectasYDeberiaDeRecibir0Puntos

--- src/test/java/poo/coders/main/ComportamientoGroupChoiceTest.java
+++ src/test/java/poo/coders/main/ComportamientoGroupChoiceTest.java
@@ -11,20 +11,37 @@ public class ComportamientoGroupChoiceTest {
 	public void test01JugadorColocaTodasCorrectasYDeberiaRecibir1Punto(){
 		Pregunta pregunta = new Pregunta("", new ComportamientoGroupChoice());
 		Jugador jugador = new Jugador("");
+		Jugador jugador2 = new Jugador("");
 
 		List<String> textoRespuestas = List.of("A", "B", "C");
 		List<String> textoRespuestasJugador = List.of("A", "B", "C");
+		List<String> textoRespuestasJugador2 = List.of("C", "A", "B");
+		InterpretadorGroupChoice interpretador = new InterpretadorGroupChoice(textoRespuestas);
+		InterpretadorGroupChoice interpretador2 = new InterpretadorGroupChoice(textoRespuestas);
+
+		jugador.procesarPregunta(pregunta, interpretador.evaluarRespuestas(textoRespuestasJugador));
+		jugador2.procesarPregunta(pregunta, interpretador2.evaluarRespuestas(textoRespuestasJugador2));
+
+		Assertions.assertEquals(1, jugador.getPuntos());
+		Assertions.assertEquals(1, jugador2.getPuntos());
+	}
+
+	@Test
+	public void test02JugadorColocaMitadDeLasRespuestasCorrectasYDeberiaDeRecibir0Puntos(){
+		//jugador coloca 2 correctas y 1 incorrecta y no da puntos
+		Pregunta pregunta = new Pregunta("", new ComportamientoGroupChoice());
+		Jugador jugador = new Jugador("");
+
+		List<String> textoRespuestas = List.of("A", "B", "C", "D");
+		List<String> textoRespuestasJugador = List.of("B", "A");
 		InterpretadorGroupChoice interpretador = new InterpretadorGroupChoice(textoRespuestas);
 
 		jugador.procesarPregunta(pregunta, interpretador.evaluarRespuestas(textoRespuestasJugador));
 
-		Assertions.assertEquals(1, jugador.getPuntos());
+		Assertions.assertEquals(0, jugador.getPuntos());
 	}
 
 
-
-	//jugador coloca 3 correctas y 0 incorrecta y debe recibir 1 punto
-	//jugador coloca 2 correctas y 1 incorrecta y no da puntos
 	//jugador coloca 3 opciones incorrectamente y no da puntos
 	//jugador coloca 2 opciones incorrectas y 1 correcta y no da puntos
 
