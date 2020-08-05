280ece64d7a6cba477db67e4a00f405d63ecc3f3
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-05 01:04:43 -0300

    test(PreguntasConMultiplicadorTest): se añadio test test01JugadorUsaMutiplicadorX2YContestaConRespuestaCorrectaGana2Puntos

---
+++ src/test/java/poo/coders/main/PreguntasConMultiplicadorTest.java
@@ -0,0 +1,21 @@
+package poo.coders.main;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+
+public class PreguntasConMultiplicadorTest {
+	@Test
+	public void test01JugadorUsaMutiplicadorX2YContestaConRespuestaCorrectaGana2Puntos() {
+		Pregunta pregunta = new Pregunta("", new ComportamientoVoFPenalidad());
+		Jugador jugador = new Jugador("");
+		ArrayList<Respuesta> respuestasJugador = new ArrayList<>();
+		respuestasJugador.add(new RespuestaCorrecta(""));
+
+		jugador.usarMultiplicadorX2();
+		jugador.procesarPregunta(pregunta, respuestasJugador);
+
+		Assertions.assertEquals(2, jugador.getPuntos());
+
+	}
+}
