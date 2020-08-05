1ab22433ddadd05219369b794e6a1c7bed12f834
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-05 19:48:17 -0300

    chore(pom): agregado openjfx, fxmloader

---
+++ src/main/java/poo/coders/main/App.java
@@ -0,0 +1,33 @@
+package poo.coders.main;
+
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class App extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws IOException {
+
+		// set up the scene
+		Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("app.fxml")));
+		Scene scene = new Scene(root);
+
+		// set up the stage
+		stage.setTitle("Orbit the Camera Around an Object Sample");
+		stage.setWidth(800);
+		stage.setHeight(700);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
