7f8b1fd108d7384f1751dafa4ba52b164c1161c8
Ezequiel Schejtman <13641204+Ezequiel-Schejtman@users.noreply.github.com>
2020-08-08 11:16:50 -0300

    refactor: Elimino Interpretador

--- src/main/java/poo/coders/main/modelo/ComportamientoOrderedChoice.java
+++ src/main/java/poo/coders/main/modelo/ComportamientoOrderedChoice.java
@@ -4,13 +4,6 @@ import java.util.List;
 
 public class ComportamientoOrderedChoice extends Comportamiento {
 
-	InterpretadorOrderedChoice interpretador;
-
-	public ComportamientoOrderedChoice(List<String> respuestasOrdenadas) {
-		this.interpretador = new InterpretadorOrderedChoice(respuestasOrdenadas);
-	}
-
-
 	@Override
 	public Puntaje crearPuntaje() {
 		return new PuntajeClasico();
