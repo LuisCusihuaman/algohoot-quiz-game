ac4a1071fec7b704aecd801f3518e14a563c2e7b
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-08 20:14:16 -0300

    feat(Exclusividad): se añadió método reiniciarExclusividad

--- src/main/java/poo/coders/main/modelo/Exclusividad.java
+++ src/main/java/poo/coders/main/modelo/Exclusividad.java
@@ -15,6 +15,10 @@ public class Exclusividad {
 		this.multiplicadorExclusividad *= 2;
 	}
 
+	public void reiniciarExclusividad() {
+		multiplicadorExclusividad = 1;
+	}
+
 	public void determinarPuntosGanados(Jugador jugador1, Jugador jugador2, int puntajeJugador1, int puntajeJugador2) {
 		if (puntajeJugador1 != puntajeJugador2) {
 			if(puntajeJugador1 > puntajeJugador2) {
