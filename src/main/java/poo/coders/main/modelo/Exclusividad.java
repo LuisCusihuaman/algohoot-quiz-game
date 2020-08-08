7d305253efe0cabb307ccb9e13eb824190cbfdfe
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-08 20:08:02 -0300

    feat(Pregunta): se añadió método aplicarConJugadores

--- src/main/java/poo/coders/main/modelo/Exclusividad.java
+++ src/main/java/poo/coders/main/modelo/Exclusividad.java
@@ -15,4 +15,13 @@ public class Exclusividad {
 		this.multiplicadorExclusividad *= 2;
 	}
 
+	public void determinarPuntosGanados(Jugador jugador1, Jugador jugador2, int puntajeJugador1, int puntajeJugador2) {
+		if (puntajeJugador1 != puntajeJugador2) {
+			if(puntajeJugador1 > puntajeJugador2) {
+				jugador1.ganarPuntaje(puntajeJugador1);
+			} else {
+				jugador2.ganarPuntaje(puntajeJugador2);
+			}
+		}
+	}
 }
