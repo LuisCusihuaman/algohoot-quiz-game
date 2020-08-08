74b2b208393da6602055d8d6dd27355d706f622d
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-08 20:12:05 -0300

    test(ExclusividadTest): se añadio test03

--- src/test/java/poo/coders/main/modelo/ExclusividadTest.java
+++ src/test/java/poo/coders/main/modelo/ExclusividadTest.java
@@ -51,7 +51,23 @@ public class ExclusividadTest {
 
 	@Test
 	public void test03JugadorUsaExclusivdadEnPreguntaVoFEligeLaOpcionIncorrectaYSuRivalLaIncorrectaGanaCeroPuntos(){
+		Jugador jugador1 = new Jugador("1");
+		Jugador jugador2 = new Jugador("2");
+		Pregunta preguntaVoF = new Pregunta("", new ComportamientoVoF());
+		Exclusividad exclusividad = new Exclusividad();
 
+		ArrayList<Respuesta> respuestasJugador1 = new ArrayList<>();
+		ArrayList<Respuesta> respuestasJugador2 = new ArrayList<>();
+
+		respuestasJugador1.add(new RespuestaIncorrecta(""));
+		respuestasJugador2.add(new RespuestaIncorrecta(""));
+
+		jugador1.activarExclusividad(exclusividad);
+
+		preguntaVoF.aplicarConJugadores(jugador1, jugador2, respuestasJugador1, respuestasJugador2);
+
+		Assertions.assertEquals(0, jugador1.getPuntos());
+		Assertions.assertEquals(0, jugador2.getPuntos());
 	}
 	@Test
 	public void test04JugadorUsaExclusivdadEnPreguntaVoFEligeLaOpcionIncorrectaYSuRivalLaCorrectaYElRivalGanaDosPuntos(){
