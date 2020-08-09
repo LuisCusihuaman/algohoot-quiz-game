c1e4ac5c83ce53884406656a6a23d0e300df7d94
matiasezequielcabral <cabralmatiasezequiel@gmail.com>
2020-08-09 18:23:55 -0300

    fix: se modifico el module-info para que corriera bien

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,15 +2,14 @@ module poo.coders {
 	requires javafx.controls;
 	requires javafx.fxml;
 
-	exports poo.coders.main;
+	//exports poo.coders.main;
 	exports poo.coders.main.modelo;
 	exports poo.coders.main.controlador;
-	//exports poo.coders.main.vista;
-	//si esta vacio te tira un error mvn
+	exports poo.coders.main.vista;
 
-	opens poo.coders.main;
+	//opens poo.coders.main;
 	opens poo.coders.main.modelo;
 	opens poo.coders.main.controlador;
-	//opens poo.coders.main.vista;
+	opens poo.coders.main.vista;
 
 }
