867ab862ecbf62a2e22f6a037280d28470d6053f
matiasezequielcabral <cabralmatiasezequiel@gmail.com>
2020-08-09 19:18:29 -0300

    feat: controlador seleccion nombre de jugador

---
+++ src/main/java/poo/coders/main/controlador/ControladorSeleccionNombreJugador.java
@@ -0,0 +1,18 @@
+package poo.coders.main.controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ControladorSeleccionNombreJugador {
+	@FXML
+	private TextField jugardor1id;
+	@FXML
+	private TextField jugardor2id;
+	@FXML
+	private Button setjugador;
+
+	@FXML
+	void setearNombre(ActionEvent event) {}
+}
