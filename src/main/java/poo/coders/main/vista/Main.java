98b54833eb4af500230eda7d7cae5895650ca899
matiasezequielcabral <cabralmatiasezequiel@gmail.com>
2020-08-09 19:29:41 -0300

    feat: main del programa

---
+++ src/main/java/poo/coders/main/vista/Main.java
@@ -0,0 +1,24 @@
+package poo.coders.main.vista;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		Parent root = FXMLLoader.load(getClass().getResource("seleccionNombreJugador"));
+		Scene scene = new Scene(root);
+
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
