298fd9c799cf6ae4309181408c70bc66dc5c1f25
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-09 18:55:30 -0300

    fix(Parser): Ahora se lee directamente un array de Preguntas Serializadas

---
+++ src/main/java/poo/coders/main/modelo/data/PreguntaSerializada.java
@@ -0,0 +1,62 @@
+
+package poo.coders.main.modelo.data;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class PreguntaSerializada {
+
+	@SerializedName("type")
+	@Expose
+	private String type;
+
+	@SerializedName("enunciado")
+	@Expose
+	private String enunciado;
+
+	@SerializedName("options")
+	@Expose
+	private List<OpcionSerializada> options = null;
+
+	@SerializedName("puntaje_type")
+	@Expose
+	private String puntajeType;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public String getEnunciado() {
+		return enunciado;
+	}
+
+	public void setEnunciado(String enunciado) {
+		this.enunciado = enunciado;
+	}
+
+
+	public List<OpcionSerializada> getOptions() {
+		return options;
+	}
+
+	public void setOptions(List<OpcionSerializada> options) {
+		this.options = options;
+	}
+
+	public String getPuntajeType() {
+		return puntajeType;
+	}
+
+	public void setPuntajeType(String puntajeType) {
+		this.puntajeType = puntajeType;
+	}
+
+
+}
