8d4ec31418c488f10e8d8db5ff242607345b5b36
matiasezequielcabral <cabralmatiasezequiel@gmail.com>
2020-08-09 19:35:02 -0300

    fix: se agrego error sintactico de compilacion

--- src/main/java/poo/coders/main/vista/Main.java
+++ src/main/java/poo/coders/main/vista/Main.java
@@ -6,12 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
 
-		Parent root = FXMLLoader.load(getClass().getResource("seleccionNombreJugador"));
+		Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("/home/matias/Escritorio/tp2-algo3/src/main/java/poo/coders/main/vista/seleccionNombreJugador.fxml")));
 		Scene scene = new Scene(root);
 
 		stage.setScene(scene);
