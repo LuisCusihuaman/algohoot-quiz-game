31954b055c86ea355045be451c90e8efd4d6081d
matiasezequielcabral <cabralmatiasezequiel@gmail.com>
2020-08-10 19:01:06 -0300

    feat: se crearon controladores para todas las vistas creadas

---
+++ src/main/java/poo/coders/main/controlador/ControladorOrderedChoice.java
@@ -0,0 +1,33 @@
+package poo.coders.main.controlador;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class ControladorOrderedChoice{
+
+	@FXML
+	private Label nombreJugador;
+	@FXML
+	private Label pregunta;
+	@FXML
+	private Label tiempo;
+	@FXML
+	private Button multiplicadorx2;
+	@FXML
+	private Button multiplicadorx3;
+	@FXML
+	private Label tipoPregunta;
+	@FXML
+	private GridPane respuesta3;
+	@FXML
+	private Label respuesta1;
+	@FXML
+	private Label respuesta2;
+	@FXML
+	private Label respuesta4;
+	@FXML
+	private Label respuesta5;
+
+}
+
---
+++ src/main/java/poo/coders/main/controlador/ControladorVerdaderoFalso.java
@@ -0,0 +1,26 @@
+package poo.coders.main.controlador;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ControladorVerdaderoFalso {
+
+	@FXML
+	private Label pregunta;
+	@FXML
+	private Label tiempo;
+	@FXML
+	private Label jugador;
+	@FXML
+	private Label tipoPregunta;
+	@FXML
+	private Button multiplicadorx2;
+	@FXML
+	private Button multiplicadorx3;
+	@FXML
+	private Label verdadero;
+	@FXML
+	private Label falso;
+
+}
+
