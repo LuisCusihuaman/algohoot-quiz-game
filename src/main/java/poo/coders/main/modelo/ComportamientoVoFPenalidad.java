f1a3acde26ece32b38111b26dbf699e3e018191d
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-09 22:07:55 -0300

    style: eliminados imports sin usar

--- src/main/java/poo/coders/main/modelo/ComportamientoVoFPenalidad.java
+++ src/main/java/poo/coders/main/modelo/ComportamientoVoFPenalidad.java
@@ -1,10 +1,6 @@
 package poo.coders.main.modelo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ComportamientoVoFPenalidad extends Comportamiento {
-
 
 	@Override
 	protected Puntaje crearPuntaje() {
--- src/main/java/poo/coders/main/modelo/Jugador.java
+++ src/main/java/poo/coders/main/modelo/Jugador.java
@@ -1,6 +1,5 @@
 package poo.coders.main.modelo;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Jugador {
