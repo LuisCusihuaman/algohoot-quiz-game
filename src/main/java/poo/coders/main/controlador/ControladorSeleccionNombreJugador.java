6ce288323bff36553ba82c9a37f118a955e9ed09
matiasezequielcabral <cabralmatiasezequiel@gmail.com>
2020-08-12 03:35:59 -0300

    chore: se le asigna el juego a los controlador

--- src/main/java/poo/coders/main/controlador/ControladorSeleccionNombreJugador.java
+++ src/main/java/poo/coders/main/controlador/ControladorSeleccionNombreJugador.java
@@ -1,5 +1,7 @@
 package poo.coders.main.controlador;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,28 +12,49 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import poo.coders.main.modelo.Juego;
 
 import java.io.IOException;
 import java.util.Objects;
 
-public class ControladorSeleccionNombreJugador {
+public class ControladorSeleccionNombreJugador implements Observable, ControladorInterfaz {
 	@FXML
-	private TextField jugardor1id;
+	private TextField jugador1id;
 	@FXML
-	private TextField jugardor2id;
+	private TextField jugador2id;
 	@FXML
 	private Button iniciarJuego;
 	@FXML
 	private Label tituloJuego;
+	private Juego juego;
+
+
+	public void setearJuego(Juego juego){
+		this.juego = juego;
+	}
 
 	@FXML
 	void setearNombre(ActionEvent event) throws IOException {
 
-		System.out.println("Clickeaste wacho.");
-		Parent view2 = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("groupChoice.fxml")));
+
+		String nombreJugador1 = jugador1id.getText();
+		String nombreJugador2 = jugador2id.getText();
+		juego.inicializar(nombreJugador1, nombreJugador2);
+		Parent view2 = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("verdaderoFalso.fxml")));
 		Scene scene2 = new Scene(view2);
 		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
 		window.setScene(scene2);
 		window.show();
+	}
+
+
+	@Override
+	public void addListener(InvalidationListener invalidationListener) {
+
+	}
+
+	@Override
+	public void removeListener(InvalidationListener invalidationListener) {
+
 	}
 }
