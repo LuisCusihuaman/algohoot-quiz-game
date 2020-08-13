8b7cc15f66fb4b2ce82a0d96e0fc5ce64aa8e868
Ezequiel Schejtman <13641204+Ezequiel-Schejtman@users.noreply.github.com>
2020-08-13 00:40:13 -0300

    feat: Implemento clase Juego

--- src/main/java/poo/coders/main/modelo/Juego.java
+++ src/main/java/poo/coders/main/modelo/Juego.java
@@ -4,38 +4,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Juego {
-
-	private Turno turno;
-	private List<Jugador> jugadores;
-	private List<Pregunta> preguntas;
-	private Estado estado;
+	//private Turno turno;
+	//private Estado estado;
+	private ArrayList<Jugador> jugadores;
+	private ArrayList<Pregunta> preguntas;
+	ArrayList<ArrayList<Opcion>> respuestasDeJugadores;
+	int indexJugadorActual;
+	int indexPreguntaActual;
 
 	public Juego() {
-		this.turno = new Turno();
+		//this.turno = new Turno();
+		//this.estado = Estado.INICIO;
 		this.jugadores = new ArrayList<>();
+		this.jugadores.add(new Jugador("placeholder"));
+		this.jugadores.add(new Jugador("placeholder"));
 		this.preguntas = new Parser().parsear();
-		this.estado = Estado.INICIO;
-	}
-
-
-	public void inicializar(String nombreJugador1, String nombreJugador2){
-		this.jugadores.add(new Jugador(nombreJugador1));
-		this.jugadores.add(new Jugador(nombreJugador2));
-		this.turno.asignarJugadores(this.jugadores);
-		this.estado = Estado.EN_JUEGO;
+		this.respuestasDeJugadores = new ArrayList<>();
+		this.respuestasDeJugadores.add(new ArrayList<>());
+		this.respuestasDeJugadores.add(new ArrayList<>());
+		this.indexJugadorActual = 0;
+		this.indexPreguntaActual = 0;
 
 	}
 
-	public String getPregunta(){
-		return preguntas.get(0).getEnunciado();
+
+	public void empezarJuego(String nombreJugador1, String nombreJugador2){
+		this.jugadores.set(0, new Jugador(nombreJugador1));
+		this.jugadores.set(1, new Jugador(nombreJugador2));
 	}
 
-	public String getNombre(){
-		return jugadores.get(0).getNombre();
+	public void siguienteTurno(ArrayList<Opcion> respuestasJugadorActual){
+		respuestasDeJugadores.set(indexJugadorActual, respuestasJugadorActual);
+		indexJugadorActual++;
+		indexJugadorActual %= jugadores.size();
+		if(indexJugadorActual == 0){
+			preguntas.get(indexPreguntaActual).darPuntosAJugadores(jugadores.get(0), jugadores.get(1), respuestasDeJugadores.get(0), respuestasDeJugadores.get(1));
+			indexPreguntaActual++;
+		}
+	}
+
+	public Jugador getJugadorActual(){
+		return jugadores.get(indexJugadorActual);
+	}
+
+	public Pregunta getPreguntaActual(){
+		return preguntas.get(indexPreguntaActual);
 	}
 
 	public String getTipoPregunta(){
-		return preguntas.get(0).getTipoPregunta();
+		return preguntas.get(indexPreguntaActual).getTipoPregunta();
 	}
 
 
