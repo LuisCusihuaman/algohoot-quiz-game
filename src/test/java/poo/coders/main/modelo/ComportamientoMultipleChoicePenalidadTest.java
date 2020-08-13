e901f5d5b133f3420af7b5a1b10e201817bccaf1
Ezequiel Schejtman <13641204+Ezequiel-Schejtman@users.noreply.github.com>
2020-08-12 22:43:15 -0300

    test(ComportamientoMultipleChoicePenlidad): Se agregan tests que validan la cantidad de opciones

--- src/test/java/poo/coders/main/modelo/ComportamientoMultipleChoicePenalidadTest.java
+++ src/test/java/poo/coders/main/modelo/ComportamientoMultipleChoicePenalidadTest.java
@@ -6,6 +6,45 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class ComportamientoMultipleChoicePenalidadTest {
+	@Test
+	public void test00_1SeCreaElComportamientoCon5OpcionesYNoLanzaException(){
+		ArrayList<Opcion> opcionesOriginales = new ArrayList<>();
+		opcionesOriginales.add(new Opcion("1", ""));
+		opcionesOriginales.add(new Opcion("2", ""));
+		opcionesOriginales.add(new Opcion("3", ""));
+		opcionesOriginales.add(new Opcion("4", ""));
+		opcionesOriginales.add(new Opcion("5", ""));
+
+		Assertions.assertDoesNotThrow(() -> {
+			ComportamientoMultipleChoicePenalidad comportamiento = new ComportamientoMultipleChoicePenalidad(opcionesOriginales);
+		});
+
+	}
+
+	@Test
+	public void test00_2SeCreaElComportamientoCon1OpcionYLanzaException(){
+		ArrayList<Opcion> opcionesOriginales = new ArrayList<>();
+		opcionesOriginales.add(new Opcion("1", ""));
+
+		Assertions.assertThrows(RuntimeException.class, () -> {
+			ComportamientoMultipleChoicePenalidad comportamiento = new ComportamientoMultipleChoicePenalidad(opcionesOriginales);
+		});
+	}
+	@Test
+	public void test00_3SeCreaElComportamientoCon6OpcionesYLanzaException(){
+		ArrayList<Opcion> opcionesOriginales = new ArrayList<>();
+		opcionesOriginales.add(new Opcion("1", ""));
+		opcionesOriginales.add(new Opcion("2", ""));
+		opcionesOriginales.add(new Opcion("3", ""));
+		opcionesOriginales.add(new Opcion("4", ""));
+		opcionesOriginales.add(new Opcion("5", ""));
+		opcionesOriginales.add(new Opcion("6", ""));
+
+		Assertions.assertThrows(RuntimeException.class, () -> {
+			ComportamientoMultipleChoicePenalidad comportamiento = new ComportamientoMultipleChoicePenalidad(opcionesOriginales);
+		});
+	}
+
 
 	@Test
 	public void test01JugadorResponde3OpcionesCorrectasYRecibe3Puntos() {
@@ -40,7 +79,6 @@ public class ComportamientoMultipleChoicePenalidadTest {
 
 	@Test
 	public void test03JugadorResponde4OpcionesIncorrectamenteYDeberiaTenerMenos4Puntos() {
-
 		Pregunta pregunta = new Pregunta("Pregunta", new ComportamientoMultipleChoicePenalidad());
 		Jugador jugador = new Jugador("Pepe");
 		ArrayList<Opcion> respuestas = new ArrayList<>();
