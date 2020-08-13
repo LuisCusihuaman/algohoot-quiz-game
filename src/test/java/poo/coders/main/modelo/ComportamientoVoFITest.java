62ac3070b30059c4e908d5230ab12bfe5edcd7ab
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-12 23:48:38 -0300

    test(ComportamientoVoFTest): se añadió tests

--- src/test/java/poo/coders/main/modelo/ComportamientoVoFITest.java
+++ src/test/java/poo/coders/main/modelo/ComportamientoVoFITest.java
@@ -3,11 +3,12 @@ package poo.coders.main.modelo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ComportamientoVoFITest {
 	@Test
-	void test02PreguntaVoFRecibeListaDeRespuestasYAsignaPuntosCorrectamente() {
+	void test01PreguntaVoFRecibeListaDeRespuestasYAsignaPuntosCorrectamente() {
 		Jugador tomas = new Jugador("Tomas");
 		Jugador kevin = new Jugador("Kevin");
 		Pregunta pregunta = new Pregunta("", new ComportamientoVoF());
@@ -21,5 +22,38 @@ public class ComportamientoVoFITest {
 
 		Assertions.assertEquals(1, tomas.getPuntos());
 		Assertions.assertEquals(0, kevin.getPuntos());
+	}
+
+	@Test
+	public void test02SeIngresanMasDeDosOpcionesEnComportamientoYLanzaExcepcion() {
+		ArrayList<Opcion> opcionesOriginales = new ArrayList<>();
+		opcionesOriginales.add(new Opcion("1", ""));
+		opcionesOriginales.add(new Opcion("2",""));
+		opcionesOriginales.add(new Opcion("3",""));
+
+		Assertions.assertThrows(RuntimeException.class, () -> {
+			ComportamientoVoF comportamiento = new ComportamientoVoF(opcionesOriginales);
+		});
+	}
+
+	@Test
+	public void test03SeIngresanMenosDeDosOpcionesYLanzaExcepcion() {
+		ArrayList<Opcion> opcionesOriginales = new ArrayList<>();
+		opcionesOriginales.add(new Opcion("1", ""));
+
+		Assertions.assertThrows(RuntimeException.class, () -> {
+			ComportamientoVoF comportamiento = new ComportamientoVoF(opcionesOriginales);
+		});
+	}
+
+	@Test
+	public void test04SeIngresanDosOpcionesYNoLanzaExcepcion() {
+		ArrayList<Opcion> opcionesOriginales = new ArrayList<>();
+		opcionesOriginales.add(new Opcion("1", ""));
+		opcionesOriginales.add(new Opcion("2", ""));
+		
+		Assertions.assertDoesNotThrow(() -> {
+			ComportamientoVoF comportamiento = new ComportamientoVoF(opcionesOriginales);
+		});
 	}
 }
