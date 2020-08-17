6cde30ac0877f95a98dce6647c1f70d1f86fb610
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-17 20:09:02 -0300

    feat: se añadió interfaz ComportamientoConExclusividad

---
+++ src/main/java/poo/coders/main/modelo/comportamientos/ComportamientoConExclusividad.java
@@ -0,0 +1,7 @@
+package poo.coders.main.modelo.comportamientos;
+
+import poo.coders.main.modelo.modificadores.Exclusividad;
+
+public interface ComportamientoConExclusividad {
+	Exclusividad activarExclusividad();
+}
