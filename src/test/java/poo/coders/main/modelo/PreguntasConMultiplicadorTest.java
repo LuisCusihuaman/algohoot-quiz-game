3bc0cac834b3ecba27b5216699e14892157c0d70
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-17 20:03:14 -0300

    test(PreguntasConMultiplicadorTest): se añadió test04

--- src/test/java/poo/coders/main/modelo/PreguntasConMultiplicadorTest.java
+++ src/test/java/poo/coders/main/modelo/PreguntasConMultiplicadorTest.java
@@ -65,4 +65,23 @@ public class PreguntasConMultiplicadorTest {
 		Assertions.assertEquals(2, jugador1.getPuntos());
 		Assertions.assertEquals(2, jugador2.getPuntos());
 	}
+
+	@Test
+	public void test04SeAgreganMultiplicadorDistintosEnAmbosJugadoresEnVoFClasicoRespondenConOpcionCorrectaGanan2PuntosY3PuntosRespectivamente() {
+		Jugador jugador1 = new Jugador("Cthulhu");
+		Jugador jugador2 = new Jugador("Nyarlathotep");
+		Pregunta preguntaVoF = new Pregunta("La pregunta", new ComportamientoVoF());
+		ArrayList<Opcion> opcionesJugador1 = new ArrayList<>();
+		ArrayList<Opcion> opcionesJugador2 = new ArrayList<>();
+		preguntaVoF.agregarMultiplicadorAJugador(jugador1, new MultiplicadorX2());
+		preguntaVoF.agregarMultiplicadorAJugador(jugador2, new MultiplicadorX3());
+		Exclusividad exclusividad = new SinExclusividad();
+		opcionesJugador1.add(new OpcionCorrecta("1","El pulpo loco"));
+		opcionesJugador2.add(new OpcionCorrecta("2", "No es buena onda"));
+
+		exclusividad.definirPuntosJugadoresEnPregunta(preguntaVoF, jugador1, jugador2, opcionesJugador1, opcionesJugador2);
+
+		Assertions.assertEquals(2, jugador1.getPuntos());
+		Assertions.assertEquals(3, jugador2.getPuntos());
+	}
 }
