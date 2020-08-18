9d80251293bf1e394b57fa06f73ca74f5a5309dc
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-18 02:22:47 -0300

    refactor: separado logica de botones

---
+++ src/main/java/poo/coders/main/vista/componentes/botones/BotonExclusividad.java
@@ -0,0 +1,14 @@
+package poo.coders.main.vista.componentes.botones;
+
+import javafx.scene.control.Button;
+
+public class BotonExclusividad extends Button {
+
+	public BotonExclusividad() {
+		super();
+		this.setLayoutX(237);
+		this.setLayoutY(20);
+		this.setText("EXCLUSIVIDAD");
+		this.setOnAction(null);
+	}
+}
