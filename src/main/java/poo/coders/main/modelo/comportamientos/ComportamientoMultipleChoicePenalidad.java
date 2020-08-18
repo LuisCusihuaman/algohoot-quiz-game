00aabb34a1295bef335d5cfe59a42cacaef8ed08
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-17 21:22:53 -0300

    refactor: se agregaron métodos para implementar en las clases Comportamiento

--- src/main/java/poo/coders/main/modelo/comportamientos/ComportamientoMultipleChoicePenalidad.java
+++ src/main/java/poo/coders/main/modelo/comportamientos/ComportamientoMultipleChoicePenalidad.java
@@ -3,10 +3,11 @@ package poo.coders.main.modelo.comportamientos;
 import poo.coders.main.modelo.Opcion;
 import poo.coders.main.modelo.Puntaje;
 import poo.coders.main.modelo.PuntajePenalidad;
+import poo.coders.main.modelo.modificadores.Exclusividad;
 
 import java.util.List;
 
-public class ComportamientoMultipleChoicePenalidad extends Comportamiento {
+public class ComportamientoMultipleChoicePenalidad extends Comportamiento implements ComportamientoConExclusividad {
 	private int valor;
 	public ComportamientoMultipleChoicePenalidad(){
 		this.valor = 1;
@@ -26,4 +27,9 @@ public class ComportamientoMultipleChoicePenalidad extends Comportamiento {
 		return "Multiple Choice Penalidad";
 	}
 
+
+	@Override
+	public Exclusividad activarExclusividad(Exclusividad exclusividad) {
+		return exclusividad.activarExclusividad();
+	}
 }
