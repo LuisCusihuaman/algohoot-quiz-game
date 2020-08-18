005fe9d5539c6974d8cdabd8cb6e60c15a8b06f5
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-18 02:22:47 -0300

    refactor: separado logica de botones

---
+++ src/main/java/poo/coders/main/vista/componentes/botones/BotonX2.java
@@ -0,0 +1,13 @@
+package poo.coders.main.vista.componentes.botones;
+
+import javafx.scene.control.Button;
+
+public class BotonX2 extends Button {
+	public BotonX2() {
+		super();
+		this.setLayoutX(120);
+		this.setLayoutY(20);
+		this.setText("X2");
+		this.setOnAction(null);
+	}
+}
---
+++ src/main/java/poo/coders/main/vista/componentes/botones/BotonX3.java
@@ -0,0 +1,13 @@
+package poo.coders.main.vista.componentes.botones;
+
+import javafx.scene.control.Button;
+
+public class BotonX3 extends Button {
+	public BotonX3() {
+		super();
+		this.setLayoutX(180);
+		this.setLayoutY(20);
+		this.setText("X3");
+		this.setOnAction(null);
+	}
+}
