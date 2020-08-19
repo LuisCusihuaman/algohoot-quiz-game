faa8462ee9887984a7f1557dd87199a228741cea
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-18 23:49:16 -0300

    fix: se corrigieron tests mal implementados

--- src/test/java/poo/coders/main/modelo/MultiplicadorTest.java
+++ src/test/java/poo/coders/main/modelo/MultiplicadorTest.java
@@ -2,9 +2,7 @@ package poo.coders.main.modelo;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import poo.coders.main.modelo.comportamientos.ComportamientoMultipleChoiceClasico;
-import poo.coders.main.modelo.comportamientos.ComportamientoMultipleChoiceParcial;
-import poo.coders.main.modelo.comportamientos.ComportamientoVoF;
+import poo.coders.main.modelo.comportamientos.*;
 import poo.coders.main.modelo.modificadores.Multiplicador;
 import poo.coders.main.modelo.modificadores.MultiplicadorX2;
 import poo.coders.main.modelo.modificadores.MultiplicadorX3;
@@ -13,8 +11,8 @@ import java.util.ArrayList;
 
 public class MultiplicadorTest {
 	@Test
-	public void test01ActivarMultiplicadorX2ConOpcionCorrectaEnVoFClasicoDevuelve2EnObtenerPuntaje() {
-		Pregunta preguntaVoF = new Pregunta("La Pregunta", new ComportamientoVoF());
+	public void test01ActivarMultiplicadorX2ConOpcionCorrectaEnVoFPenalidadDevuelve2EnObtenerPuntaje() {
+		Pregunta preguntaVoF = new Pregunta("La Pregunta", new ComportamientoVoFPenalidad());
 		ArrayList<Opcion> opciones = new ArrayList<>();
 		Multiplicador multiplicador = new MultiplicadorX2();
 		opciones.add(new OpcionCorrecta("1","Esta opción es correcta"));
@@ -24,7 +22,7 @@ public class MultiplicadorTest {
 
 	@Test
 	public void test02ActivarMultiplicadorX3ConOpcionCorrectaEnVoFClasicoDevuelve3EnObtenerPuntaje() {
-		Pregunta preguntaVoF = new Pregunta("La Pregunta", new ComportamientoVoF());
+		Pregunta preguntaVoF = new Pregunta("La Pregunta", new ComportamientoVoFPenalidad());
 		ArrayList<Opcion> opciones = new ArrayList<>();
 		Multiplicador multiplicador = new MultiplicadorX3();
 		opciones.add(new OpcionCorrecta("1","Esta opción es correcta"));
@@ -34,18 +32,18 @@ public class MultiplicadorTest {
 
 	@Test
 	public void test03ActivarMultiplicadorX2ConVariasOpcionesCorrectasEnMultipleChoiceParcialDevuelveElDobleDePuntosEnObtenerPuntaje() {
-		Pregunta preguntaVoF = new Pregunta("La Pregunta", new ComportamientoMultipleChoiceParcial());
+		Pregunta preguntaMultipleChoicePenalidad = new Pregunta("La Pregunta", new ComportamientoMultipleChoicePenalidad());
 		ArrayList<Opcion> opciones = new ArrayList<>();
 		Multiplicador multiplicador = new MultiplicadorX2();
 		opciones.add(new OpcionCorrecta("1","Esta opción es correcta"));
 		opciones.add(new OpcionCorrecta("2","Esta opción es correcta"));
 
-		Assertions.assertEquals(4, preguntaVoF.obtenerPuntaje(opciones, multiplicador));
+		Assertions.assertEquals(4, preguntaMultipleChoicePenalidad.obtenerPuntaje(opciones, multiplicador));
 	}
 
 	@Test
-	public void test03ActivarMultiplicadorX2ConVariasOpcionesCorrectasEnMultipleChoiceParcialDevuelveElTripleDePuntosEnObtenerPuntaje() {
-		Pregunta preguntaVoF = new Pregunta("La Pregunta", new ComportamientoMultipleChoiceParcial());
+	public void test03ActivarMultiplicadorX3ConVariasOpcionesCorrectasEnMultipleChoicePenalidadDevuelveElTripleDePuntosEnObtenerPuntaje() {
+		Pregunta preguntaVoF = new Pregunta("La Pregunta", new ComportamientoMultipleChoicePenalidad());
 		ArrayList<Opcion> opciones = new ArrayList<>();
 		Multiplicador multiplicador = new MultiplicadorX3();
 		opciones.add(new OpcionCorrecta("1","Esta opción es correcta"));
@@ -55,25 +53,23 @@ public class MultiplicadorTest {
 	}
 
 	@Test
-	public void test04ActivarMultiplicadorX2ConVariasOpcionesCorrectasEnMultipleChoiceClasicoDevuelveElDobleDePuntosEnObtenerPuntaje() {
-		Pregunta preguntaVoF = new Pregunta("La Pregunta", new ComportamientoMultipleChoiceClasico());
+	public void test04ActivarMultiplicadorX2ConOpcionIncorrectaEnVoFPenalidadDevuelveMenosElDobleDePuntosEnObtenerPuntaje() {
+		Pregunta preguntaVoF = new Pregunta("La Pregunta", new ComportamientoVoFPenalidad());
 		ArrayList<Opcion> opciones = new ArrayList<>();
 		Multiplicador multiplicador = new MultiplicadorX2();
-		opciones.add(new OpcionCorrecta("1","Esta opción es correcta"));
-		opciones.add(new OpcionCorrecta("2","Esta opción es correcta"));
+		opciones.add(new OpcionIncorrecta("1","Esta opción es correcta, minga"));
 
-		Assertions.assertEquals(2, preguntaVoF.obtenerPuntaje(opciones, multiplicador));
+		Assertions.assertEquals(-2, preguntaVoF.obtenerPuntaje(opciones, multiplicador));
 	}
 
 	@Test
-	public void test05ActivarMultiplicadorX2ConVariasOpcionesCorrectasEnMultipleChoiceClasicoDevuelveElTripleDePuntosEnObtenerPuntaje() {
-		Pregunta preguntaVoF = new Pregunta("La Pregunta", new ComportamientoMultipleChoiceClasico());
+	public void test05ActivarMultiplicadorX3ConOpcionIncorrectaEnVoFPenalidadDevuelveMenosElTripleDePuntosEnObtenerPuntaje() {
+		Pregunta preguntaVoF = new Pregunta("La Pregunta", new ComportamientoVoFPenalidad());
 		ArrayList<Opcion> opciones = new ArrayList<>();
 		Multiplicador multiplicador = new MultiplicadorX3();
-		opciones.add(new OpcionCorrecta("1","Esta opción es correcta"));
-		opciones.add(new OpcionCorrecta("2","Esta opción es correcta"));
+		opciones.add(new OpcionIncorrecta("1","Esta opción es correcta, minga"));
 
-		Assertions.assertEquals(3, preguntaVoF.obtenerPuntaje(opciones, multiplicador));
+		Assertions.assertEquals(-3, preguntaVoF.obtenerPuntaje(opciones, multiplicador));
 	}
 
 }
