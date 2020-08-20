0445aea1d272393fe2b0e7dc644984e89390e016
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-20 02:07:04 -0300

    refactor(ComportamientoVoFPenalidad): se refactorizaron los tests

--- src/test/java/poo/coders/main/modelo/ComportamientoVoFPenalidadITest.java
+++ src/test/java/poo/coders/main/modelo/ComportamientoVoFPenalidadITest.java
@@ -7,50 +7,23 @@ import poo.coders.main.modelo.comportamientos.ComportamientoVoFPenalidad;
 import java.util.ArrayList;
 
 class ComportamientoVoFPenalidadITest {
-	/*
-		@Test
-		void deberiaPreguntaVoFPenalidadCrearseCorrectamenteIndicandoleCualEsLaRespuestaCorrecta() {
-			Pregunta             pregunta        = new Pregunta("Este es el mejor TP.", new ComportamientoVoFPenalidad());
-			RespuestaCorrecta    correcta        = new RespuestaCorrecta();
-			ArrayList<Respuesta> listaRespuestas = new ArrayList<>();
-			listaRespuestas.add(correcta);
-			Puntos diferencialPuntos = pregunta.obtenerPuntaje(listaRespuestas);
-			Assertions.assertEquals(1, diferencialPuntos.getCantidadPuntos());
-		}
 
-		@Test
-		void jugadorCon5puntosRespondeIncorrectamente2vecesYSuPuntajeDeberiaSer3() {
-			Jugador              jugador          = new Jugador("Alan");
-			Pregunta             pregunta         = new Pregunta("El software es tangible.", new ComportamientoVoFPenalidad());
-			ArrayList<Respuesta> respuestaJugador = new ArrayList<>();
-			respuestaJugador.add(new RespuestaIncorrecta(""));
-			respuestaJugador.add(new RespuestaIncorrecta(""));
-			jugador.procesarPregunta(pregunta, respuestaJugador);
-			Assertions.assertEquals(3, jugador.getPuntos());
-		}
-	*/
 	@Test
-	public void test01JugadorRespondeConUnaIncorrectaTienePuntajeMenos1() {
-		Jugador jugador = new Jugador("");
+	public void test01ObtenerPuntajeConUnaOpcionCorrectaEnComportamientoVoFPenalidadDevuelveUnPunto() {
 		Pregunta pregunta = new Pregunta("", new ComportamientoVoFPenalidad());
-		ArrayList<Opcion> respuestasJugador = new ArrayList<>();
-		respuestasJugador.add(new OpcionIncorrecta("Incorrecta", ""));
+		ArrayList<Opcion> opcionesElegidasPorJugador = new ArrayList<>();
+		opcionesElegidasPorJugador.add(new OpcionConjunto("Incorrecta", "","1"));
 
-		jugador.procesarPregunta(pregunta, respuestasJugador);
-
-		Assertions.assertEquals(-1, jugador.getPuntos());
+		Assertions.assertEquals(-1, pregunta.obtenerPuntaje(opcionesElegidasPorJugador));
 	}
 
 	@Test
-	public void test02JugadorRespondeCon1CorrectaTienePuntaje1() {
-		Jugador jugador = new Jugador("");
+	public void test02ObtenerPuntajeConUnaOpcionIncorrectaEnComportamientoVoFPenalidadDevuelveMenosUnPunto() {
 		Pregunta pregunta = new Pregunta("", new ComportamientoVoFPenalidad());
-		ArrayList<Opcion> respuestasJugador = new ArrayList<>();
-		respuestasJugador.add(new OpcionCorrecta("Correcta", ""));
+		ArrayList<Opcion> opcionesElegidasPorJugador = new ArrayList<>();
+		opcionesElegidasPorJugador.add(new OpcionConjunto("2", "","1"));
 
-		jugador.procesarPregunta(pregunta, respuestasJugador);
-
-		Assertions.assertEquals(1, jugador.getPuntos());
+		Assertions.assertEquals(-1, pregunta.obtenerPuntaje(opcionesElegidasPorJugador));
 	}
 
 	@Test
