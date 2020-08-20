4750f6bbe6ac26ccc9c9c62a2f9156a7e1e7e61a
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-20 14:16:00 -0300

    chore: agregado id unicos a los botones

--- src/main/java/poo/coders/main/vista/componentes/botones/BotonExclusividad.java
+++ src/main/java/poo/coders/main/vista/componentes/botones/BotonExclusividad.java
@@ -7,6 +7,7 @@ public class BotonExclusividad extends Button {
 
 	public BotonExclusividad(MultiplicadorExclusividadHandlerBoton multiplicadorExclusividadHandlerBoton) {
 		super();
+		this.setId("BOTONEXCLUSIVIDAD");
 		this.setLayoutX(237);
 		this.setLayoutY(20);
 		this.setText("EXCLUSIVIDAD");
