6e8cf366080f416cf86880298aa5e3d2813b79e5
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-20 01:51:20 -0300

    fix: se arreglaron tests defectuosos

--- src/test/java/poo/coders/main/modelo/ComportamientoMultipleChoicePenalidadTest.java
+++ src/test/java/poo/coders/main/modelo/ComportamientoMultipleChoicePenalidadTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public class ComportamientoMultipleChoicePenalidadTest {
 	@Test
-	public void test00_1SeCreaElComportamientoCon5OpcionesYNoLanzaException(){
+	public void test01SeCreaElComportamientoCon5OpcionesYNoLanzaException(){
 		ArrayList<Opcion> opcionesOriginales = new ArrayList<>();
 		opcionesOriginales.add(new Opcion("1", ""));
 		opcionesOriginales.add(new Opcion("2", ""));
@@ -23,7 +23,7 @@ public class ComportamientoMultipleChoicePenalidadTest {
 	}
 
 	@Test
-	public void test00_2SeCreaElComportamientoCon1OpcionYLanzaException(){
+	public void test02SeCreaElComportamientoCon1OpcionYLanzaException(){
 		ArrayList<Opcion> opcionesOriginales = new ArrayList<>();
 		opcionesOriginales.add(new Opcion("1", ""));
 
@@ -32,7 +32,7 @@ public class ComportamientoMultipleChoicePenalidadTest {
 		});
 	}
 	@Test
-	public void test00_3SeCreaElComportamientoCon6OpcionesYLanzaException(){
+	public void test03SeCreaElComportamientoCon6OpcionesYLanzaException(){
 		ArrayList<Opcion> opcionesOriginales = new ArrayList<>();
 		opcionesOriginales.add(new Opcion("1", ""));
 		opcionesOriginales.add(new Opcion("2", ""));
@@ -48,51 +48,40 @@ public class ComportamientoMultipleChoicePenalidadTest {
 
 
 	@Test
-	public void test01JugadorResponde3OpcionesCorrectasYRecibe3Puntos() {
+	public void test04ObtenerPuntajeCon3OpcionesCorrectasEnMultipleChoicePenalidadDevuelve3Puntos() {
 
 		Pregunta pregunta = new Pregunta("Pregunta", new ComportamientoMultipleChoicePenalidad());
-		Jugador jugador = new Jugador("Pepe");
-		ArrayList<Opcion> respuestas = new ArrayList<>();
-		respuestas.add(new OpcionCorrecta("Correcta", ""));
-		respuestas.add(new OpcionCorrecta("Correcta", ""));
-		respuestas.add(new OpcionCorrecta("Correcta", ""));
+		//Jugador jugador = new Jugador("Pepe");
+		ArrayList<Opcion> opcionesElegidasPorJugador = new ArrayList<>();
+		opcionesElegidasPorJugador.add(new OpcionConjunto("1", "Opcion 1", "1"));
+		opcionesElegidasPorJugador.add(new OpcionConjunto("1", "Opcion 2", "1"));
+		opcionesElegidasPorJugador.add(new OpcionConjunto("1", "Opcion 3", "1"));
 
-		jugador.procesarPregunta(pregunta, respuestas);
-
-		Assertions.assertEquals(3, jugador.getPuntos());
+		Assertions.assertEquals(3, pregunta.obtenerPuntaje(opcionesElegidasPorJugador));
 	}
 
 	@Test
-	public void test02JugadorResponde2OpcionesCorrectasY2IncorrectasRecibe2Puntos() {
+	public void test05ObtenerPuntajeCon3OpcionesCorrectasYUnaIncorrectaEnMultipleChoicePenalidadDevuelve2Puntos() {
 
 		Pregunta pregunta = new Pregunta("Pregunta", new ComportamientoMultipleChoicePenalidad());
-		Jugador jugador = new Jugador("Pepe");
-		ArrayList<Opcion> respuestas = new ArrayList<>();
-		respuestas.add(new OpcionCorrecta("Correcta", ""));
-		respuestas.add(new OpcionIncorrecta("Incorrecta", ""));
-		respuestas.add(new OpcionCorrecta("Correcta", ""));
-		respuestas.add(new OpcionIncorrecta("Incorrecta", ""));
+		ArrayList<Opcion> opcionesElegidasPorJugador = new ArrayList<>();
+		opcionesElegidasPorJugador.add(new OpcionConjunto("1", "Opcion Correcta","1"));
+		opcionesElegidasPorJugador.add(new OpcionConjunto("2","Opcion Correcta", "2"));
+		opcionesElegidasPorJugador.add(new OpcionConjunto("3", "Opcion Correcta","3"));
+		opcionesElegidasPorJugador.add(new OpcionConjunto("4","OpcionIncorrecta","3"));
 
-		jugador.procesarPregunta(pregunta, respuestas);
-
-		Assertions.assertEquals(0, jugador.getPuntos());
+		Assertions.assertEquals(2, pregunta.obtenerPuntaje(opcionesElegidasPorJugador));
 	}
 
 	@Test
-	public void test03JugadorResponde4OpcionesIncorrectamenteYDeberiaTenerMenos4Puntos() {
+	public void test05ObtenerPuntajeCon4OpcionesIncorrectasEnMultipleChoicePenalidadDevuelveMenos4Puntos() {
 		Pregunta pregunta = new Pregunta("Pregunta", new ComportamientoMultipleChoicePenalidad());
-		Jugador jugador = new Jugador("Pepe");
-		ArrayList<Opcion> respuestas = new ArrayList<>();
+		ArrayList<Opcion> opcionesElegidasPorJugador = new ArrayList<>();
+		opcionesElegidasPorJugador.add(new OpcionConjunto("1", "","2"));
+		opcionesElegidasPorJugador.add(new OpcionConjunto("2", "","1"));
+		opcionesElegidasPorJugador.add(new OpcionConjunto("3", "","4"));
+		opcionesElegidasPorJugador.add(new OpcionConjunto("4", "","3"));
 
-		respuestas.add(new OpcionIncorrecta("Incorrecta", ""));
-		respuestas.add(new OpcionIncorrecta("Incorrecta", ""));
-		respuestas.add(new OpcionIncorrecta("Incorrecta", ""));
-		respuestas.add(new OpcionIncorrecta("Incorrecta", ""));
-
-		jugador.procesarPregunta(pregunta, respuestas);
-
-		Assertions.assertEquals((-4), jugador.getPuntos());
+		Assertions.assertEquals((-4), pregunta.obtenerPuntaje(opcionesElegidasPorJugador));
 	}
-
-
 }
