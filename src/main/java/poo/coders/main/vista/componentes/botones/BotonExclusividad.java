422c9c4aef9051d08000dd912aa6cd632ee575be
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-20 02:56:03 -0300

    feat: eventhandlers de modificadores de puntajes

--- src/main/java/poo/coders/main/vista/componentes/botones/BotonExclusividad.java
+++ src/main/java/poo/coders/main/vista/componentes/botones/BotonExclusividad.java
@@ -1,14 +1,15 @@
 package poo.coders.main.vista.componentes.botones;
 
 import javafx.scene.control.Button;
+import poo.coders.main.controlador.Botones.MultiplicadorExclusividadHandlerBoton;
 
 public class BotonExclusividad extends Button {
 
-	public BotonExclusividad() {
+	public BotonExclusividad(MultiplicadorExclusividadHandlerBoton multiplicadorExclusividadHandlerBoton) {
 		super();
 		this.setLayoutX(237);
 		this.setLayoutY(20);
 		this.setText("EXCLUSIVIDAD");
-		this.setOnAction(null);
+		this.setOnAction(multiplicadorExclusividadHandlerBoton);
 	}
 }
